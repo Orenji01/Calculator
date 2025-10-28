51f065e863fb613198a8b304207d7ad5649ebc39
LACRIM94 <anasmestour@gmail.com>
2025-10-28 15:23:57 +0100

    Make functions for basic math operators

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ function add(a, b) {
 function sub(a, b) {
     if (a > b) {
         return a - b;
+    } else if (b > a) {
+        return b - a
     }
 }
 
-function multiply
+function multiply(a, b) {
+    return a * b;
+}
+
+const divide = (a, b) => a / b;
